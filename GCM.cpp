499937d7c85412084ffeffec2f02fa27a7b121dd
Mohammad Adel Alsoheil <alsoheilm@gmail.com>
2024-12-07 16:57:10 +0200

    added decryption for sequential GCM

--- GCM.cpp
+++ GCM.cpp
@@ -15,7 +15,7 @@ private:
     ByteVector IV;
     ByteVector AAD;
 
-    void prepareCounter(ByteVector& counter, const ByteVector& IV) {
+    void prepareCounter(ByteVector &counter, const ByteVector &IV) {
         // If IV is 96 bits, append 0x00000001 to form J0
         if (IV.size() == 12) {
             counter = IV;
@@ -29,9 +29,9 @@ private:
     }
 
 
-
-    void incrementCounter(ByteVector& counter) {
-        for (int i = 15; i >= 12; --i) { // Last 4 bytes represent the counter
+    void incrementCounter(ByteVector &counter) {
+        for (int i = 15; i >= 12; --i) {
+            // Last 4 bytes represent the counter
             if (++counter[i] != 0) {
                 break; // Stop incrementing if no overflow
             }
@@ -39,12 +39,12 @@ private:
     }
 
 
-    vector<ByteVector> GCTR(ByteVector ICB, ByteVector val){
+    vector<ByteVector> GCTR(ByteVector ICB, ByteVector val) {
         AES aes;
         ByteVector CB = ICB;
-        vector<ByteVector> X = nest(val,16);
+        vector<ByteVector> X = nest(val, 16);
         vector<ByteVector> res;
-        for(int i = 0;i<X.size();++i){
+        for (int i = 0; i < X.size(); ++i) {
             ByteVector Y;
             Y = xorF(aes.encrypt(CB, key), X[i]);
             incrementCounter(CB);
@@ -52,13 +52,12 @@ private:
         }
 
         return res;
-
     }
 
-    ByteVector GHASH(ByteVector val, ByteVector H){
+    ByteVector GHASH(ByteVector val, ByteVector H) {
         ByteVector Y0 = ByteVector(16, 0x00);
-        vector<ByteVector> X = nest(val,16);
-        for(int i = 0;i<X.size();++i){
+        vector<ByteVector> X = nest(val, 16);
+        for (int i = 0; i < X.size(); ++i) {
             Y0 = Ghash::gf128Multiply(xorF(Y0, X[i]), H);
         }
         return Y0;
@@ -91,7 +90,6 @@ private:
 
         res.insert(res.end(), lenC64.begin(), lenC64.end());
 
-        cout << bytesToHex(res) << endl;
 
         return res;
     }
@@ -106,13 +104,11 @@ private:
         return encoded;
     }
 
-
 public:
-
-
     // for psuedo code reference https://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
-    pair<ByteVector, ByteVector> encrypt(const ByteVector key,  const ByteVector IV, const ByteVector AAD,ByteVector P) {
-        this->AAD= AAD;
+    pair<ByteVector, ByteVector> encrypt(const ByteVector key, const ByteVector IV, const ByteVector AAD,
+                                         ByteVector P) {
+        this->AAD = AAD;
         this->IV = IV;
         this->key = key;
 
@@ -130,40 +126,81 @@ public:
         ByteVector newC = flatten(C);
 
         //removes padding
-        while (sizeOfPlainText<newC.size()) {
+        while (sizeOfPlainText < newC.size()) {
             newC.pop_back();
             sizeOfPlainText++;
         }
 
-        int sizeOfCinBits = newC.size()*8;
-        int sizeofAADinBits = AAD.size()*8;
+        int sizeOfCinBits = newC.size() * 8;
+        int sizeofAADinBits = AAD.size() * 8;
 
-        int u = (128*ceil((double) sizeOfCinBits/128)) - sizeOfCinBits;
-        int v = (128*ceil((double) sizeofAADinBits/128)) - sizeofAADinBits;
+        int u = (128 * ceil((double) sizeOfCinBits / 128)) - sizeOfCinBits;
+        int v = (128 * ceil((double) sizeofAADinBits / 128)) - sizeofAADinBits;
 
-        cout << u << " " << v << endl;
-        cout << sizeOfCinBits<< " " << sizeofAADinBits<< endl;
-        ByteVector S = GHASH(padC(newC,u,v, sizeOfCinBits,sizeofAADinBits ), H);
+        ByteVector S = GHASH(padC(newC, u, v, sizeOfCinBits, sizeofAADinBits), H);
 
         prepareCounter(J0, this->IV);
-        vector<ByteVector> T = GCTR(J0,S);
+        vector<ByteVector> T = GCTR(J0, S);
         ByteVector newT = flatten(T);
 
 
-
-        return { newC, newT };
+        return {newC, newT};
     }
 
+    ByteVector decrypt(const ByteVector key, const ByteVector IV, const ByteVector AAD, ByteVector C, ByteVector T) {
+        // outputs plainText
+        this->AAD = AAD;
+        this->IV = IV;
+        this->key = key;
 
+        AES aes;
+        int sizeOfCipherText = C.size();
+        ByteVector H = aes.encrypt(ByteVector(16, 0x00), key);
+
+
+        ByteVector J0;
+        prepareCounter(J0, this->IV);
+
+
+        incrementCounter(J0);
+        vector<ByteVector> P = GCTR(J0, C);
+        ByteVector newP = flatten(P);
+
+        //removes padding
+        while (sizeOfCipherText < newP.size()) {
+            newP.pop_back();
+            sizeOfCipherText++;
+        }
+
+        int sizeOfCinBits = C.size() * 8;
+        int sizeofAADinBits = AAD.size() * 8;
+
+        int u = (128 * ceil((double) sizeOfCinBits / 128)) - sizeOfCinBits;
+        int v = (128 * ceil((double) sizeofAADinBits / 128)) - sizeofAADinBits;
+
+        ByteVector S = GHASH(padC(C, u, v, sizeOfCinBits, sizeofAADinBits), H);
+
+        prepareCounter(J0, this->IV);
+        vector<ByteVector> Tprime = GCTR(J0, S);
+        ByteVector newTprime = flatten(Tprime);
+
+        for (int i = 0; i < 16; ++i) {
+            if (T[i] != newTprime[i]) {
+                throw invalid_argument("There is no integrity between T and Tprime");
+            }
+        }
+
+        return newP;
+    }
 };
 
-int main(){
+int main() {
     // Key (16 bytes)
     ByteVector Key = {
-            0x4C, 0x97, 0x3D, 0xBC, 0x73, 0x64, 0x62, 0x16,
-            0x74, 0xF8, 0xB5, 0xB8, 0x9E, 0x5C, 0x15, 0x51,
-            0x1F, 0xCE, 0xD9, 0x21, 0x64, 0x90, 0xFB, 0x1C,
-            0x1A, 0x2C, 0xAA, 0x0F, 0xFE, 0x04, 0x07, 0xE5
+        0x4C, 0x97, 0x3D, 0xBC, 0x73, 0x64, 0x62, 0x16,
+        0x74, 0xF8, 0xB5, 0xB8, 0x9E, 0x5C, 0x15, 0x51,
+        0x1F, 0xCE, 0xD9, 0x21, 0x64, 0x90, 0xFB, 0x1C,
+        0x1A, 0x2C, 0xAA, 0x0F, 0xFE, 0x04, 0x07, 0xE5
     };
     // P (Plaintext, 64 bytes)
     ByteVector P = {
@@ -179,22 +216,25 @@ int main(){
 
     // IV (Initialization Vector, 12 bytes)
     ByteVector IV = {
-            0x7A, 0xE8, 0xE2, 0xCA, 0x4E, 0xC5, 0x00, 0x01,
-            0x2E, 0x58, 0x49, 0x5C
+        0x7A, 0xE8, 0xE2, 0xCA, 0x4E, 0xC5, 0x00, 0x01,
+        0x2E, 0x58, 0x49, 0x5C
     };
 
     // A (Associated Data, 20 bytes)
     ByteVector A = {
-            0x68, 0xF2, 0xE7, 0x76, 0x96, 0xCE, 0x7A, 0xE8,
-            0xE2, 0xCA, 0x4E, 0xC5, 0x88, 0xE5, 0x4D, 0x00,
-            0x2E, 0x58, 0x49, 0x5C
+        0x68, 0xF2, 0xE7, 0x76, 0x96, 0xCE, 0x7A, 0xE8,
+        0xE2, 0xCA, 0x4E, 0xC5, 0x88, 0xE5, 0x4D, 0x00,
+        0x2E, 0x58, 0x49, 0x5C
     };
 
 
     GCM gcm;
-    pair<ByteVector, ByteVector> res = gcm.encrypt(Key, IV, A,P);
+    pair<ByteVector, ByteVector> res = gcm.encrypt(Key, IV, A, P);
+    cout << "Plain Text: " <<bytesToHex(P) << endl;
     cout << "Cipher Text: " + bytesToHex(res.first) << "\n";
     cout << "Added Tag: " + bytesToHex(res.second) << "\n";
+    ByteVector deciphered = gcm.decrypt(Key,IV,A,res.first,res.second);
+    cout << "Decrypted Text: " + bytesToHex(deciphered) << "\n";
 
     return 0;
 }
